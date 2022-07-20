3d355d3606e008010326872a662eac1f693278dd
ROSH <95622188+roshnirajak@users.noreply.github.com>
2022-07-20 18:39:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function addwork(){
+    console.log("Heloo");
+    let work= document.getElementById('work').value;
+    let display= document.getElementById('display');
+
+    display.innerHTML=work;
+}
