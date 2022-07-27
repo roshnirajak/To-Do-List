1b0dc4d3d87ed164aae37dcdb05f250d6ff80dea
ROSH <95622188+roshnirajak@users.noreply.github.com>
2022-07-28 03:54:55 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # To-Do-List
+<a href="https://roshnirajak.github.io/To-Do-List/">Visit Site</a><br>
+Preview <br>
+<img src="preview-img.png">
--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-function addwork(){
+function getAndUpdate() {
     console.log("Heloo");
-    let work= document.getElementById('work').value;
-    let display= document.getElementById('display');
+    let work = document.getElementById('work').value;
 
-    if(localStorage.getItem('jsonElem')==null){
-        jsonArray=[];
+    if (localStorage.getItem('jsonElem') == null) {
+        jsonArray = [];
         jsonArray.push(work);
         localStorage.setItem('jsonElem', JSON.stringify(jsonArray))
     }
-    return false;
+    else {
+        jsonArrayStr = localStorage.getItem('jsonElem');
+        jsonArray = JSON.parse(jsonArrayStr);
+        jsonArray.push(work);
+        localStorage.setItem('jsonElem', JSON.stringify(jsonArray));
+    }
+    addwork();
 }
+addwork();
+function addwork() {
+    if (localStorage.getItem('jsonElem') == null) {
+        jsonArray = [];
+        localStorage.setItem('jsonElem', JSON.stringify(jsonArray))
+    }
+    else {
+        jsonArrayStr = localStorage.getItem('jsonElem');
+        jsonArray = JSON.parse(jsonArrayStr);
+    }
+    let tbody = document.getElementById('tbody');
+    let item = "";
+    jsonArray.forEach((element, index) => {
+        item += `
+        <tr>
+        <td>${index + 1}</td>
+        <td>${element}</td>
+        <td><svg xmlns="http://www.w3.org/2000/svg" onclick="return remove(${index})" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg></td>
+    </tr> `;
+    });
+    tbody.innerHTML = item;
+}
+function remove(index) {
+    jsonArrayStr = localStorage.getItem('jsonElem');
+    jsonArray = JSON.parse(jsonArrayStr);
+    //remove item
+    jsonArray.splice(index, 1);
+    localStorage.setItem('jsonElem', JSON.stringify(jsonArray));
+    addwork();
+}
+
